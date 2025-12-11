93c682400f1eeba8e68e954c64eae2cb516c19d3
DominikFischli <dominikfischli@bluewin.ch>
2025-12-11 12:18:01 +0100

    fix: report if initial load fails

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -15,8 +15,12 @@ const configStore = useConfigStore()
 
 const loadConfig = axios.get('/api/config')
 const loadData = axios.get('/api')
+console.log(loadConfig)
+console.log(loadData)
 
 const loading = ref(true)
+const loadError = ref(false)
+
 Promise.all([loadData, loadConfig])
   .then(([resData, resConfig]) => {
     // Initialize data store
@@ -28,18 +32,29 @@ Promise.all([loadData, loadConfig])
     configStore.initialize(config)
 
     loading.value = false
+    loadError.value = false
   })
   .catch((err) => {
     console.error('Error initializing data: ', err)
+    loading.value = false
+    loadError.value = true
   })
 </script>
 
 <template>
   <main class="h-full m-auto">
     <Loading v-if="loading" class="mt-30" />
-    <div v-else class="flex justify-center gap-3 w-5/6 m-auto mt-1">
-      <Groups class="w-full min-w-2xl" />
-      <Generator class="w-full" />
+    <div v-else>
+      <div
+        v-if="loadError"
+        class="text-lg text-center border m-5 p-5 bg-rose-200 text-rose-950 border-rose-400 rounded-2xl"
+      >
+        Loading failed.
+      </div>
+      <div v-else class="flex justify-center gap-3 w-5/6 m-auto mt-1">
+        <Groups class="w-full min-w-2xl" />
+        <Generator class="w-full" />
+      </div>
     </div>
   </main>
 </template>
